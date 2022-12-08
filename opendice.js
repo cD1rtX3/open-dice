28078b758688657f83db22952d700d378f100481
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2022-12-08 16:50:44 -0500

    Update opendice.js

--- opendice.js
+++ opendice.js
@@ -1,1 +1,19 @@
-
+// Copyright COArSe_D1RTxxx under GPLv3 on 8 December 2022
+class Dice {
+	constructor (t, n) {
+		switch (typeof t) {
+			case "number":
+				//
+				break
+			case "object":
+				if (Array.isArray(t)) {
+					//
+				} else {
+					//
+				}
+				break
+			default:
+				throw new Error("Die must be a number, array, or die")
+		}
+	}
+}
