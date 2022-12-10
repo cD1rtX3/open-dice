96f711427318786775986f44eedb82b4099d9dd1
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2022-12-09 21:57:36 -0500

    Update opendice.js

--- opendice.js
+++ opendice.js
@@ -1,9 +1,33 @@
 // Copyright COArSe_D1RTxxx under GPLv3 on 8 December 2022
+function convolve (a, b) {
+	if (Array.isArray(a) && Array.isArray(b)) {
+		let c = []
+		let d
+		for (let i = 0; i < a.length + b.length - 1; i++) {
+			d = 0
+			for (let j = 0; i - j > -1; j++) {
+				d += a[j] * b[i-j]
+			}
+			c.push(d)
+		}
+		return c
+	} else {
+		return undefined
+	}
+}
 class Dice {
+	a = []
+	hist = 1
+	m;
 	constructor (t, n) {
 		switch (typeof t) {
 			case "number":
-				//
+				m = n
+				a = [...Array(n).keys()].map(x => 1)
+				b = [...Array(n).keys()].map(x => 1)
+				for (let i = 1; i < n; i++) {
+					a = convolve(a, b)
+				}
 				break
 			case "object":
 				if (Array.isArray(t)) {
