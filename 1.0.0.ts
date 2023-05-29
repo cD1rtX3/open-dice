e1657f1a03af7355b06dd954ec3163c6be0cc701
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2023-05-29 13:10:19 -0400

    Update 1.0.0.ts
    _d.sort(x, m?)

--- 1.0.0.ts
+++ 1.0.0.ts
@@ -1,17 +1,30 @@
 // Licensed by Coarse Rosinflower under GPLv3 on 8 December 2022
 
 class _d {
-	static convolve (n:any, o:any) {
-		if (typeof n == "object") { if (typeof o == "object") {
-			//
+	static sort (x:number[]):number[]; static sort (x:number[], a:boolean):object;
+	static sort (x:number[], a?:boolean):object { // shell sort
+		if (x.length == 0) {return (a ? [[], []] : [])}
+		let arr = x, n:number[] = [...(Array(x.length).keys())], t:number
+		const gaps = [1750, 701, 301, 132, 57, 23, 10, 4, 1]
+		if (a) {
+			gaps.forEach(function(g){
+				let b = true
+				while (b) {b = false; for (let i = g; i < x.length; i++) {
+					if (arr[i] < arr[i - g]) {t = arr[i]; arr[i] = arr[i - g]; arr[i - g] = t; t = n[i]; n[i] = n[i - g]; n[i - g] = t; b = true}
+				}}
+			})
 		} else {
-			throw new Error((typeof o) + "type passed as second argument to convolve, expected an object")
-		}} else {
-			throw new Error((typeof n) + "type passed as first argument to convolve, expected an object")
+			gaps.forEach(function(g){
+				let b = true
+				while (b) {b = false; for (let i = g; i < x.length; i++) {
+					if (arr[i] < arr[i - g]) {t = arr[i]; arr[i] = arr[i - g]; arr[i - g] = t; b = true}
+				}}
+			})
 		}
+		return (a ? [arr, n] : arr)
 	}
-	//
-	constructor (n:number, o?:number) {
+	static convolve (x:number[], y:number[]):number[]; static convolve (x:_d, y:_d):_d;
+	static convolve (x:object, y:object):object {
 		//
 	}
 }
