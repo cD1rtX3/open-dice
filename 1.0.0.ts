8ef81c442d148a476bc38aebf4aa7a7003432b0d
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2023-06-15 23:40:57 -0400

    Update 1.0.0.ts

--- 1.0.0.ts
+++ 1.0.0.ts
@@ -2,21 +2,21 @@
 
 class _d {
 	static sort (x:number[]):number[]; static sort (x:number[], a:boolean):object; static sort (x:number[], a:number[]):number[][];
-	static sort (x:number[], a?:any):object { // shell sort
+	static sort (x:number[], a?:any):object {
 		if (x.length == 0) {return (a ? [[], []] : [])}
-		let arr = x, n:number[], t:number
+		let arr = x, n:number[], t:number, b:boolean
 		if (Array.isArray(a)) {n = a} else {n = [...(Array(x.length).keys())]}
 		const gaps = [1750, 701, 301, 132, 57, 23, 10, 4, 1]
 		if (a) {
 			gaps.forEach(function(g){
-				let b = true
+				b = true
 				while (b) {b = false; for (let i = g; i < x.length; i++) {
 					if (arr[i] < arr[i - g]) {t = arr[i]; arr[i] = arr[i - g]; arr[i - g] = t; t = n[i]; n[i] = n[i - g]; n[i - g] = t; b = true}
 				}}
 			})
 		} else {
 			gaps.forEach(function(g){
-				let b = true
+				b = true
 				while (b) {b = false; for (let i = g; i < x.length; i++) {
 					if (arr[i] < arr[i - g]) {t = arr[i]; arr[i] = arr[i - g]; arr[i - g] = t; b = true}
 				}}
@@ -26,12 +26,34 @@ class _d {
 	}
 	static convolve (x:number[], y:number[]):number[]; static convolve (x:_d, y:_d):_d;
 	static convolve (x:object, y:object):object {
-		if (Array.isArray(x)) {
+		if (x instanceof Array) {
 			let a = [...Array(x.length + y.length - 1)].map(n => 0)
 			for (let i = 0; i < x.length; i++) {for (let j = 0; j < y.length; j++) {a[i + j] += x[i] * y[j]}}
 			return a
 		} else {
 			//
+		}
+	}
+	nums: [], prob: [] baseNums: [], baseProb: [], length: 0
+	constructor (x:number, n?:number); constructor (x:number[], n?:number); constructor (x:_d, n?:number); constructor ();
+	constructor (x?:any, n?:number) {
+		if (typeof x == "undefined") {} else {
+			if (n < 1 || n % 1 > 0) { // Specifically designed NOT to throw on undefined
+				throw new Error("Second argument must be a positive integer or undefined.")
+			} else {
+				if (typeof x == "number") {
+					this.baseProb = [...Array(x)].map(() => 1/x)
+					let b = this.baseProb
+					this.baseNums = [...[...Array(x)].keys()].map(m => m + 1)
+					for (i = 1; i < n; i++) {
+						b = convolve(b, this.baseProb)
+					}
+					this.prob = b
+					this.nums = [...[...Array(b.length)].keys()].map(m => m + 1)
+				} else if (x instanceof Array) {
+					//
+				} else {}
+			}
 		}
 	}
 }
