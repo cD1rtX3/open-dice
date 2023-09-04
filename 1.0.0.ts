d7fb9b0d83add072880e15cdf83867ad4d572269
cD1rtX3 <113859223+cD1rtX3@users.noreply.github.com>
2023-09-04 16:20:24 -0400

    Update 1.0.0.ts

--- 1.0.0.ts
+++ 1.0.0.ts
@@ -1,4 +1,21 @@
-// Licensed by Coarse Rosinflower under LGPLv2.1 on 4 September 2023
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \
+*   open-dice: Probability calculator for dice!                                      *
+*   Copyright (C) 2023  Coarse Rosinflower                                           *
+*                                                                                    *
+*   This library is free software; you can redistribute it and/or                    *
+*   modify it under the terms of the GNU Lesser General Public                       *
+*   License as published by the Free Software Foundation; either                     *
+*   version 2.1 of the License, or (at your option) any later version.               *
+*                                                                                    *
+*   This library is distributed in the hope that it will be useful,                  *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+*   Lesser General Public License for more details.                                  *
+*                                                                                    *
+*   You should have received a copy of the GNU Lesser General Public                 *
+*   License along with this library; if not, write to the Free Software              *
+*   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA   *
+\ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 "use strict";
 
